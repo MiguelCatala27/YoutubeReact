b6f3ae9543fa29926a6f7790b62722b5a050c08f
Miguel Catala <miguelcatala27@yahoo.com>
2021-05-19 18:22:37 -0400

    working on api

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,44 @@
-import { Component} from 'react'
-
+import { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+require("dotenv").config()
 
 const Home = () => {
-    return (
+  const [videoShow, setvideoShow] = useState([]);
+  const [userInput, setuserInput] = useState("");
 
-        <h1>Yo</h1>
-    )
-}
+  const fetchVideos = async (e) => {
+      e.preventDefault()
+    try {
+      const res = await axios.get(
+        `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}&part=snippet&type=video&q=${userInput}`
+      );
+      debugger;
+    } catch (error) {console.log(error)}
+    
+  };
 
-export default Home
+//   useEffect(() => {
+//     fetchVideos();
+//   });
+
+  const handleInput = (e) => {
+    setuserInput(e.target.value);
+  };
+  return (
+    <div>
+      <h1>Search for some videos</h1>
+      <form onSubmit={fetchVideos}>
+        <input
+          type="text"
+          placeholder="search for a video"
+          id="video"
+          onChange={handleInput}
+        />
+        <button type="submit" value="search">Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
