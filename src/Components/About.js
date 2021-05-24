a1ca9d2e1aacc740c3290ffe4cafb77f720ae697
Miguel Catala <miguelcatala27@yahoo.com>
2021-05-24 18:55:10 -0400

    about updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,7 +12,12 @@ export default function About() {
       </p>
       <h2>Created By:</h2>
       <h4>Miguel Catala</h4>
-      <p>Miguel Catala Description</p>
+      <p>I am a Software Engineer with various ideas for video games and web designs. My goal is to create an easygoing and friendly experience for all users to enjoy!</p>
+      <a href="https://github.com/MiguelCatala27" target='_blank' rel="noreferrer">GitHub</a>
+      <br>
+      </br>
+      <a href="https://www.linkedin.com/in/miguel-catala-44a730202/" target='_blank' rel="noreferrer">LinkedIn</a>
+
       <h4>Carlo Echeverri</h4>
       <p>Carlo Echeverri Description</p>
       <h4>Miguel Garcia</h4>
